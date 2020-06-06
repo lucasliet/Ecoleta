82dd8d6e188583d457d7d17af93cd8746e3cba8a
Lucas Souza <lucasouliveira@gmail.com>
2020-06-05 23:05:48 -0300

    Updates

---
+++ precache-manifest.cb942f3a4bf0cb122733289989f01f6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7f209f976ae402371f8349ab429e1",
+    "url": "/Ecoleta/index.html"
+  },
+  {
+    "revision": "fba81186e53e88ce8012",
+    "url": "/Ecoleta/static/css/main.57cbb924.chunk.css"
+  },
+  {
+    "revision": "d43b88f9e93dc3fe5785",
+    "url": "/Ecoleta/static/js/2.9e43c1fc.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/Ecoleta/static/js/2.9e43c1fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba81186e53e88ce8012",
+    "url": "/Ecoleta/static/js/main.43271d0a.chunk.js"
+  },
+  {
+    "revision": "59fe3747c8b73294707c",
+    "url": "/Ecoleta/static/js/runtime-main.680626ff.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/Ecoleta/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/Ecoleta/static/media/logo.47c4581a.svg"
+  }
+]);
