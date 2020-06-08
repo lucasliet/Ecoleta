3fe8b1e2ea34d505fb98fee7e451ed0507d73bb7
Lucas Souza <lucasouliveira@gmail.com>
2020-06-08 00:43:51 -0300

    heroku api call

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://cafezinho.sytes.net:3333'
+    //baseURL: 'http://cafezinho.sytes.net:3333'
+    baseURL: 'https://ecoleta-points-api.herokuapp.com/'
 });
 export default api;
