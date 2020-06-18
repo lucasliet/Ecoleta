5f7e2828ffb46181217a9a973c38cee8c1ed3a2b
Lucas Souza <lucasouliveira@gmail.com>
2020-06-18 14:37:13 -0300

    sqlite connection for dev tests env

--- api/src/database/connection.ts
+++ api/src/database/connection.ts
@@ -1,15 +1,28 @@
 import knex from 'knex';
+import path from 'path';
 
-const connection = knex({
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  migrations: {
-    directory: './migrations'
-  },
-  seeds: {
-    directory: './seeds'
-  },
-  useNullAsDefault: true
-})
+const knexConfig = (process.env.NODE_ENV === 'development'
+  ? {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'database.sqlite')
+    },
+    useNullAsDefault: true,
+  }
+  : {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
+  }
+)
+
+
+const connection = knex(knexConfig);
 
 export default connection;
